ae52164d5a74c6d35f9352f7f92c947680a9f21e
katie <katiesun138@gmail.com>
2022-12-05 17:25:09 -0800

    adding 400 error detection

--- webServer.py
+++ webServer.py
@@ -5,6 +5,8 @@ import sys
 serverPort = 3002
 serverSocket = socket(AF_INET, SOCK_STREAM)  # this creates a socket object
 
+# serverSocket.settimeout(0.01)
+
 serverSocket.bind(('', serverPort))  # binds to the port
 serverSocket.listen(1)  # waits for 1 client connection
 print("the server port is binded to port: ", serverPort)
@@ -18,25 +20,35 @@ while True:
         message = connectionSocket.recv(1024)
         # print("WHATTTTTT STATUS:", message.status_code)
 
-        print(message, '::', message.split()[0], ':', message.split()[1])
+        # print(message, '::', message.split()[0], ':', message.split()[1])
         filename = message.split()[1]
-        print("EEEEEK", filename, "DONEEE")
+        # print("EEEEEK", filename, "DONEEE")
 
-        print(filename, '||', filename[1:])
+        # print(filename, '||', filename[1:])
         f = open(filename[1:])
         outputdata = f.read()
-        print(outputdata)
+        # print(outputdata)
 
         # Send one HTTP header line into socket
         connectionSocket.send(bytes('\nHTTP/1.1 200 OK\n\n', 'UTF-8'))
 
-        # Send the content of the file to the client        
+        # Send the content of the file to the client
         connectionSocket.send(bytes(outputdata, 'UTF-8'))
         connectionSocket.close()
-    except IOError:
+
+    except socket.timeout:
+        connectionSocket.send(
+            bytes('\nHTTP/1.1 408 Request Timeout\n\n', 'UTF-8'))
+        connectionSocket.close()
+
+    except FileNotFoundError:
         # Send response message for file not found
         connectionSocket.send(bytes("HTTP/1.1 404 Not Found\r\n", 'UTF-8'))
-        connectionSocket.send(bytes("Content-Type: text/html\r\n",'UTF-8'))
+        connectionSocket.send(bytes("Content-Type: text/html\r\n", 'UTF-8'))
         connectionSocket.send(bytes("\r\n", 'UTF-8'))
-        connectionSocket.send(bytes("<html><head></head><body><h1>404 Not Found</h1></body></html>\r\n", 'UTF-8'))
+        connectionSocket.send(bytes(
+            "<html><head></head><body><h1>404 Not Found</h1></body></html>\r\n", 'UTF-8'))
+        connectionSocket.close()
+    except IOError:
+        connectionSocket.send(bytes('\nHTTP/1.1 400 Bad Request\n\n', 'UTF-8'))
         connectionSocket.close()
